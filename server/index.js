c034039b81fef3d06396634e665043b5f33bf00e
sdg258 <nsdu.17ck1@gmail.com>
2023-11-18 20:37:39 -0800

    init database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
+const dbconn = require('./config/dbconnect');
 
 const app = express()
 app.use(cors({
@@ -8,6 +9,8 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true }))
+
+dbconn()
 
 const PORT = process.env.PORT || 3000
 
